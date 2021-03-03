ca3d8355aee092143c04b47ace0f4ebb3206db7b
mexSocialMiner <alexandre.oliveira@socialminer.com>
2021-03-03 11:01:59 -0300

    fix script

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 var interval = setInterval(eraseWindow, 300);
+console.log('Start Interval!')
+
+function stopInterval(){
+  setInterval(interval, 60000);
+  console.log('Start Interval!')
+  console.log('Stop Interval!')
+}
 
 function eraseWindow(){
-  if(document.querySelectorAll('[data-role="toast-container"]')){
+  if(document.querySelectorAll('[data-role="toast-container"]')[0]){
       document.querySelectorAll('[data-role="toast-container"]')[0].remove();
       clearInterval(interval);
+      console.log('Erased!');
+      var interval = setInterval(eraseWindow, 300);
+      stopInterval();
   }
   if(document.querySelector('.modal-GUK9cvUQ')){
     document.querySelector('.modal-GUK9cvUQ').remove();
     clearInterval(interval);
+    console.log('Erased!');
+    var interval = setInterval(eraseWindow, 300);
+    stopInterval();
 }
 }
